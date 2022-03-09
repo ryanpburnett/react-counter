1fd2c64d96b5fc8871009545fda817abd579050a
Ryan <ryanpburnett@yahoo.com>
2022-03-09 16:28:04 -0600

    working on css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0 
+        count: 0, 
+        pancakes: 'https://static.onecms.io/wp-content/uploads/sites/43/2022/02/16/21014-Good-old-Fashioned-Pancakes-mfs_001.jpg'
     };
 
     render() { 
         return (
             <div>
+                <img className="small" src={this.state.pancakes} alt="" />
                 <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </div>
