4d31bcea8c33b2d08baff38065f0ea8d4d7c15c9
Ryan <ryanpburnett@yahoo.com>
2022-03-11 17:25:23 -0600

    dynamically rendered classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0, 
-        pancakes: 'https://static.onecms.io/wp-content/uploads/sites/43/2022/02/16/21014-Good-old-Fashioned-Pancakes-mfs_001.jpg'
+        count: 1, 
     };
 
     render() { 
         return (
             <div>
-                <img className="small" src={this.state.pancakes} alt="" />
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="space btn btn-primary">Increment</button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "space badge ";
+        classes += (this.state.count === 0) ? "badge-warning" : "badge-secondary";
+        return classes;
     }
 
     formatCount() {
