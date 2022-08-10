52c5cfc6245de1ee9d518fe83c59683af8e6fc5d
Ryan <ryanpburnett@yahoo.com>
2022-08-10 15:31:45 -0500

    broke into header and counter components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+
+    render() { 
+        return (
+            <div>
+                <img src="./favicon.ico" alt="logo" className="space"/>
+                <br />
+            </div>
+        );
+    }
+
+}
+
+export default Header;
