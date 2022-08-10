9757ab6a86bb6bc118d2e34bed4ca7d235f6fd79
Ryan <ryanpburnett@yahoo.com>
2022-08-10 16:30:24 -0500

    fix() method added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,19 +6,35 @@ class Header extends Component {
     render() { 
         return (
             <div>
-                <img src="./favicon.ico" alt="logo" id="atom" className="headerSpace" onMouseOver={this.animate}/>
-                <p id="oops">You knocked it over!</p>
+                <img src="./favicon.ico" alt="logo" id="atom" className="headerSpace" onMouseOver={this.break}/>
+                <p id="oops"></p>
+                <button id="button" className="btn-warning headerSpace" onClick={this.fix}>fix</button>
             </div>
         );
     }
 
-    animate() {
+    break() {
         let atom = document.getElementById("atom");
         let oops = document.getElementById("oops");
+        let button = document.getElementById("button");
 
         atom.style.rotate = "20deg";
         oops.style.display = "inline";
+        oops.innerText = "You knocked it over!"
+        button.style.display = "inline";
     }
+
+    fix() {
+        let atom = document.getElementById("atom");
+        let oops = document.getElementById("oops");
+        let button = document.getElementById("button");
+
+        atom.style.rotate = "0deg";
+        oops.innerText = "whew...";
+        button.style.display = "none";
+    }
+
+
 
 }
 
