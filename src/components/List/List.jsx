126b218d7cd1c15f02d82807e3ea2e391b55bf85
Ryan <ryanpburnett@yahoo.com>
2022-08-10 17:31:13 -0500

    List component added

---
+++ src/components/List/List.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Component {
+    state = { 
+        breakfast: ['Pancakes', 'Waffles', 'Churros'],
+    };
+    
+    render() { 
+        return (
+            <div>
+                <button onClick={this.addListItem} className="bisque space btn btn-primary">Add List Item</button>
+                <input id="addItemInput" type="text" />
+                <ul>
+                    {this.renderList()}
+                </ul>
+            </div>
+        );
+    }
+    
+    // list
+    renderList() {
+        let { breakfast } = this.state;
+        if (breakfast.length === 0) {
+            return <p>No items in list</p>;
+        }else{
+            let key = 0;
+            return breakfast.map(item => <li key={key++}>{item}</li>);
+        }
+    }
+    
+    addListItem = () => {
+        console.log("item added lol");
+
+        let addItemInput = document.getElementById("addItemInput");
+        let { breakfast } = this.state;
+        let newBreakfast = breakfast;
+
+        addItemInput.value !== "" ? newBreakfast.push(addItemInput.value) : alert("List item can't be blank");
+        addItemInput.value = "";
+        this.setState({breakfast: newBreakfast});
+    }
+
+}
+
+export default List;
