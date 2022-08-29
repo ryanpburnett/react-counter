961550065b75079cc5426eaa456ce9787d24a3a0
Ryan <ryanpburnett@yahoo.com>
2022-08-29 10:34:55 -0500

    sig variable made

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css';
 
+    const sig = "-RPB";
+
     const Footer = () => { 
         return (
             <div>
-                <p id="sig" className='space'>-RPB</p>
+                <p id="sig" className='space'>{sig}</p>
             </div>
         );
     }
